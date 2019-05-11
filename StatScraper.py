67ceccf7eff65ca48b6ef1766b7b03be3784a9cc
Ashank <yohaw1@gmail.com>
2019-05-10 22:41:11 -0400

    organized code into function

--- StatScraper.py
+++ StatScraper.py
@@ -2,57 +2,61 @@ import json
 import urllib.request as urllib2
 from bs4 import BeautifulSoup
 
-try: 
-    from googlesearch import search 
-except ImportError:  
-    print("No module named 'google' found") 
-  
-#query = "site:ufcstats.com jon jones"
-query = "site:ufcstats.com conor mcgregor"
+def getWebPage(name):
+    try: 
+        from googlesearch import search 
+    except ImportError:  
+        print("No module named 'google' found") 
 
-for j in search(query, tld="co.in", num=10, stop=1, pause=2): 
-    url = j
+    query = "site:ufcstats.com " + name
 
-page = urllib2.urlopen(url)
-soup = BeautifulSoup(page, 'html.parser')
+    for j in search(query, tld="co.in", num=10, stop=1, pause=2): 
+        url = j
 
-allStatsNamesAndNums = []
+    page = urllib2.urlopen(url)
+    soup = BeautifulSoup(page, 'html.parser')
+    return soup
 
-name = str(soup.h2.span.string).strip()
-allStatsNamesAndNums.append(["Name", name])
-nick = str(soup.p.string).strip()
-allStatsNamesAndNums.append(["Nick", nick])
+def getStats(soup):
+    allStatsNamesAndNums = []
 
-ul = soup.find_all('ul')
+    name = str(soup.h2.span.string).strip()
+    allStatsNamesAndNums.append(["Name", name])
+    nick = str(soup.p.string).strip()
+    allStatsNamesAndNums.append(["Nick", nick])
 
-i = 0
-for j in ul: 
-    prevLi = ul[i].li 
-    name = None #rename variable
-    if prevLi.i and prevLi.i.string:
-        name = str(prevLi.i.string).strip()
+    ul = soup.find_all('ul')
 
-    data = str(prevLi.contents[2]).strip()
-    if name and data: 
-        allStatsNamesAndNums.append([name,data])
+    i = 0
+    for j in ul: 
+        prevLi = ul[i].li 
+        name = None #rename variable
+        if prevLi.i and prevLi.i.string:
+            name = str(prevLi.i.string).strip()
 
-    for m in j:
-        nextLi = prevLi.find_next_sibling('li')
-        if nextLi is None or nextLi.i is None: #nextLi.i is accounting for the last li.i group that has unwanted information like "Fighters". Also if there is name for a stat then there must be a number for it so just need to ensure the name exists
-            break
-        name = str(nextLi.i.string).strip()
-        data = str(nextLi.contents[2]).strip()
-        #print(name + "," + data)
-        if not name and data: 
-            continue
+        data = str(prevLi.contents[2]).strip()
+        if name and data: 
+            allStatsNamesAndNums.append([name,data])
 
-        allStatsNamesAndNums.append([name,data]) 
+        for m in j:
+            nextLi = prevLi.find_next_sibling('li')
+            if nextLi is None or nextLi.i is None: #nextLi.i is accounting for the last li.i group that has unwanted information like "Fighters". Also if there is name for a stat then there must be a number for it so just need to ensure the name exists
+                break
+            name = str(nextLi.i.string).strip()
+            data = str(nextLi.contents[2]).strip()
+            if not name and data: 
+                continue
 
-        prevLi = nextLi
-    i = i+1
+            allStatsNamesAndNums.append([name,data]) 
 
-statDictionary = {stat[0]: stat[1] for stat in allStatsNamesAndNums} #creating python dictionary from list of data
+            prevLi = nextLi
+        i = i+1
 
-jsonStats = json.dumps(statDictionary, indent=4) #create json object from python dictionary
-print(jsonStats)
+    statDictionary = {stat[0]: stat[1] for stat in allStatsNamesAndNums} #creating python dictionary from list of data
+    jsonStats = json.dumps(statDictionary, indent=4) #create json object from python dictionary
+    return jsonStats
 
+
+soup = getWebPage("Jon Jones")
+stats = getStats(soup)
+print(stats)
