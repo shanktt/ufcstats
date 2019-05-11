04d0dbcec2cbf13181a6198c94f640cfeb9a5ed9
Ashank <yohaw1@gmail.com>
2019-05-11 18:57:03 -0400

    Simplified loop logic

--- StatScraper.py
+++ StatScraper.py
@@ -18,41 +18,23 @@ def getWebPage(name):
     return soup
 
 def getStats(soup):
-    allStatsNamesAndNums = []
+    stats = []
 
     name = str(soup.h2.span.string).strip()
-    allStatsNamesAndNums.append(["Name", name])
+    stats.append(["Name", name])
     nick = str(soup.p.string).strip()
-    allStatsNamesAndNums.append(["Nick", nick])
+    stats.append(["Nick", nick])
 
-    ul = soup.find_all('ul')
+    ulList = soup.find_all('ul')
 
-    i = 0
-    for j in ul: 
-        prevLi = ul[i].li 
-        name = None #rename variable
-        if prevLi.i and prevLi.i.string:
-            name = str(prevLi.i.string).strip()
+    for ul in ulList: #accessing each individual ul tag in ulList
+        for li in ul.find_all('li'): #iterating through each li tag in each ul tag
+            if li.i and (str(li.i.string).strip() != ""): #Since and short circuits wont run into error of prevLi.i not existing and trying to get a string from it. 
+                statName = str(li.i.string).strip() 
+                statNum = str(li.contents[2]).strip() #If there is a statName inside the i tag there is always an associated statNum outside the i tag but in the li tag
+                stats.append([statName,statNum])
 
-        data = str(prevLi.contents[2]).strip()
-        if name and data: 
-            allStatsNamesAndNums.append([name,data])
-
-        for m in j:
-            nextLi = prevLi.find_next_sibling('li')
-            if nextLi is None or nextLi.i is None: #nextLi.i is accounting for the last li.i group that has unwanted information like "Fighters". Also if there is name for a stat then there must be a number for it so just need to ensure the name exists
-                break
-            name = str(nextLi.i.string).strip()
-            data = str(nextLi.contents[2]).strip()
-            if not name and data: 
-                continue
-
-            allStatsNamesAndNums.append([name,data]) 
-
-            prevLi = nextLi
-        i = i+1
-
-    statDictionary = {stat[0]: stat[1] for stat in allStatsNamesAndNums} #creating python dictionary from list of data
+    statDictionary = {stat[0]: stat[1] for stat in stats} #creating python dictionary from list of data
     jsonStats = json.dumps(statDictionary, indent=4) #create json object from python dictionary
     return jsonStats
 
